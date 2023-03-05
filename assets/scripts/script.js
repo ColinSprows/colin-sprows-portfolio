5307f4efd7209f059ac66d8878133a7cc912a7e5
Colin Sprows <colin.sprows@gmail.com>
2023-03-04 19:40:15 -0500

    styled modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,3 +3,15 @@ const timeDiv = document.querySelector("#dayjs")
 const now = dayjs();
 
 timeDiv.innerHTML = now.format('H:mm')
+
+let $modal = $("#myModal");
+$modal.draggable({
+  handle: ".modal-header",
+});
+
+$modal.resizable({
+    minWidth: 500, 
+    maxWidth: 800,
+    minHeight: 400, 
+    maxHeight: 600,
+});
